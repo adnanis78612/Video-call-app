83ebbad8aea18e83e107eb56b4bd24e0b015a5ef
adnanis78612 <adnanis78612@gmail.com>
2026-07-24 23:38:45 +0530

    neww

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+    const connectionDb = await mongoose.connect("mongodb+srv://adnanis78612:adnanis78612@cluster0.xym5lsu.mongodb.net/?appName=Cluster0")
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
